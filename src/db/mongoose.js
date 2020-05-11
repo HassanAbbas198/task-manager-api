7d8ffff45bb680f0c14d895fddd66c6dbcf445e3
Hassan <hassan.abbascs@gmail.com>
2020-05-12 01:22:59 +0300

    initial commit

---
+++ src/db/mongoose.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+mongoose
+	.connect(
+		'mongodb+srv://Hassan:TpdvQ4ruOg29iNxZ@cluster0-rbus3.mongodb.net/task-manager?retryWrites=true&w=majority',
+		{ useNewUrlParser: true, useUnifiedTopology: true }
+	)
+	.then(() => {
+		console.log('Connected to MongoDB');
+	})
+	.catch(() => {
+		console.log('Failed!');
+	});
