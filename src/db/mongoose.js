72950cdcbae38c307cd3f3fd762b3e472348a44d
Hassan <hassan.abbascs@gmail.com>
2020-05-28 15:00:40 +0300

    env variables

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 
 mongoose
-	.connect(
-		`mongodb+srv://Hassan:${process.env.MONGO_ATLAS_PW}@cluster0-rbus3.mongodb.net/task-manager?retryWrites=true&w=majority`,
-		{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }
-	)
+	.connect(process.env.MONGO_URL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useFindAndModify: false,
+	})
 	.then(() => {
 		console.log('Connected to MongoDB');
 	})
