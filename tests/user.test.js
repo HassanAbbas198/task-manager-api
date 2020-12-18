1e7f784f36f244c0b13a3a0707727bac0bcd71e4
Hassan <hassan.abbascs@gmail.com>
2020-12-18 22:19:26 +0200

    finished user test suite

--- tests/user.test.js
+++ tests/user.test.js
@@ -102,3 +102,38 @@ test('Should delete account for user', async () => {
 test('Should not delete account for unauthenticated user', async () => {
 	await request(app).delete('/users/me').send().expect(401);
 });
+
+test('Should upload avatar image', async () => {
+	await request(app)
+		.post('/users/me/image')
+		.set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+		.attach('image', 'tests/fixtures/profile-pic.jpg')
+		.expect(200);
+
+	// we cant use toBe to compare objects
+	const user = await User.findById(userOneId);
+	expect(user.image).toEqual(expect.any(Buffer));
+});
+
+test('Should update valid user fields', async () => {
+	await request(app)
+		.patch('/users/me')
+		.set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+		.send({
+			name: 'Jessy',
+		})
+		.expect(200);
+
+	const user = await User.findById(userOneId);
+	expect(user.name).toEqual('Jessy');
+});
+
+test('Should not update invalid user fields', async () => {
+	await request(app)
+		.patch('/users/me')
+		.set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+		.send({
+			location: 'Beirut',
+		})
+		.expect(400);
+});
